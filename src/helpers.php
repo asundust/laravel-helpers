1eacbdc6608663db5f2d550cb70d758128a2db8d
asundust <asundust@qq.com>
2022-07-16 08:11:51 +0800

    调整函数 添加函数

--- src/helpers.php
+++ src/helpers.php
@@ -5,7 +5,8 @@ if (!function_exists('da')) {
      * dd打印封装 不断点
      * 如果能转成toArray()则转成数组.
      *
-     * @param mixed $args
+     * @param ...$args
+     * @return void
      */
     function da(...$args)
     {
@@ -19,14 +20,18 @@ if (!function_exists('da')) {
     }
 }
 
-if (!function_exists('dad')) {
+if (!function_exists('dda')) {
     /**
      * dd打印封装 并断点
      * 如果能转成toArray()则转成数组.
      *
      * @param mixed $args
      */
-    function dad(...$args)
+    /**
+     * @param ...$args
+     * @return void
+     */
+    function dda(...$args)
     {
         da(...$args);
         exit(1);
@@ -47,34 +52,34 @@ if (!function_exists('ma')) {
     }
 }
 
-if (!function_exists('mad')) {
+if (!function_exists('mda')) {
     /**
      * 移动版dd打印封装 并断点
      * 如果能转成toArray()则转成数组.
      *
      * @param mixed $args
      */
-    function mad(...$args)
+    function mda(...$args)
     {
         ma(...$args);
         exit(1);
     }
 }
 
-if (!function_exists('money_show')) {
+if (!function_exists('number_format')) {
     /**
-     * 金额格式化.
+     * 数字格式化.
      *
-     * @param mixed $money  金额数
-     * @param int   $number 小数位数
+     * @param mixed $number 数字
+     * @param int $decimal 保留小数位数
      */
-    function money_show($money, $number = 2): string
+    function number_format($number, $decimal = 2)
     {
-        if (null == $money || '' == $money) {
+        if (null == $number || '' == $number) {
             return '0.00';
         }
 
-        return sprintf('%01.'.$number.'f', $money);
+        return sprintf('%01.' . $decimal . 'f', $number);
     }
 }
 
@@ -82,11 +87,12 @@ if (!function_exists('pluck_to_array')) {
     /**
      * [$id$ => $value$, ...] 转成 [['id' => $id$, 'value' => $value$], ...] 方法.
      *
-     * @param        $array
+     * @param $array
      * @param string $value
      * @param string $key
+     * @return array
      */
-    function pluck_to_array($array, $value = 'value', $key = 'id'): array
+    function pluck_to_array($array, $value = 'value', $key = 'id')
     {
         if (method_exists($array, 'toArray')) {
             $array = $array->toArray();
@@ -103,20 +109,66 @@ if (!function_exists('pluck_to_array')) {
     }
 }
 
-if (!function_exists('ql')) {
+if (!function_exists('log_i')) {
     /**
-     * @param        $message
+     * 快速日志打印 - 详细.
+     * log_info => log_i.
+     *
+     * @param array|string|null $message 日志信息
+     * @param string|null $name 日志文件名
+     * @param string|null $path 日志写入路径
+     * @param int $max 该目录下最大日志文件数
+     */
+    function log_i($message = '', $name = 'test', $path = '', $max = 14)
+    {
+        if (0 == strlen($path)) {
+            $path = $name;
+        }
+        config([
+            'logging.channels.' . $path . '_' . $name => [
+                'driver' => 'daily',
+                'path' => storage_path('logs/' . $path . '/' . $name . '.log'),
+                'level' => 'debug',
+                'days' => $max,
+            ],
+        ]);
+        $type = '';
+        if (function_exists('debug_backtrace') && debug_backtrace()) {
+            $first = Illuminate\Support\Arr::first(debug_backtrace());
+            if (is_array($first) && isset($first['file']) && isset($first['line'])) {
+                $str = substr(str_replace(base_path(), '', $first['file']), 1);
+                $type = "{$str}:{$first['line']}";
+            }
+        }
+        if (!is_array($message)) {
+            logger()->channel($path . '_' . $name)->info($type . PHP_EOL . $message);
+        } else {
+            logger()->channel($path . '_' . $name)->info($type);
+            logger()->channel($path . '_' . $name)->info($message);
+        }
+    }
+}
+
+if (!function_exists('log_s')) {
+    /**
+     * 快速日志打印 - 简单.
+     * log_sample => log_s.
+     *
+     * @param string|array|null $message
      * @param string $path
      * @param string $name
      */
-    function ql($message, $path = '', $name = 'log')
+    function log_s($message, $path = '', $name = 'log')
     {
-        if ($path) {
-            $path = trim($path, '/').'/';
-            create_dir(storage_path('logs/'.$path));
+        if (method_exists($message, 'toArray')) {
+            $message = var_export($message->toArray(), true);
         }
-        $handle = fopen(storage_path('logs/'.$path.$name.'-'.date('Y-m-d').'.log'), 'a');
-        fwrite($handle, $message."\n");
+        if ($path) {
+            $path = trim($path, '/') . '/';
+            create_dir(storage_path('logs/' . $path));
+        }
+        $handle = fopen(storage_path('logs/' . $path . $name . '-' . date('Y-m-d') . '.log'), 'a');
+        fwrite($handle, $message . "\n");
         fclose($handle);
     }
 }
@@ -127,7 +179,7 @@ if (!function_exists('create_dir')) {
      *
      * @param string $path 文件夹路径
      */
-    function create_dir(string $path)
+    function create_dir($path)
     {
         if (!file_exists($path)) {
             create_dir(dirname($path));
@@ -151,7 +203,7 @@ if (!function_exists('console_line')) {
             ];
             $code = $types[$type] ?? '37';
             // 30黑色，31红色，32绿色，33黄色，34蓝色，35洋红，36青色，37白色，
-            echo chr(27).'['.$code.'m'."$text".chr(27).'[0m'.PHP_EOL;
+            echo chr(27) . '[' . $code . 'm' . "$text" . chr(27) . '[0m' . PHP_EOL;
         }
     }
 }
