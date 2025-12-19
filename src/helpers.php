0a4869a4c09fde0af4c38eee07a44d5f3ad41a5a
塵世不再 <asundust@qq.com>
2025-12-19 08:44:15 +0800

    Handle empty string in explode function

--- src/helpers.php
+++ src/helpers.php
@@ -129,7 +129,7 @@ if (!function_exists('string_to_array')) {
         foreach ($replaces as $replace) {
             $string = str_replace($replace, $separator, $string);
         }
-        $items = array_filter(array_unique(explode($separator, $string)));
+        $items = array_filter(array_unique(explode($separator, $string ?: '')));
         foreach ($items as &$item) {
             $item = trim($item);
         }
